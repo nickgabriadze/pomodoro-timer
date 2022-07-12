01735b317f655f335db95ed2b8e6f9f342f23ecb
Nika Gabriadze <67961868+nickgabriadze@users.noreply.github.com>
2022-07-12 09:13:27 +0400

    Add files via upload

---
+++ build/precache-manifest.b702e2eea28560866ce7f504c3ddc22b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d2a9cc78c5665ab6ae8",
+    "url": "/static/css/main.5af8a3b7.chunk.css"
+  },
+  {
+    "revision": "6d2a9cc78c5665ab6ae8",
+    "url": "/static/js/main.6d2a9cc7.chunk.js"
+  },
+  {
+    "revision": "655805e31a4602310207",
+    "url": "/static/js/1.655805e3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f540cff790495611fb04f2fac5f1903",
+    "url": "/index.html"
+  }
+];
